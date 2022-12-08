58cd92157d43b820b03d52e29d456ef6aa51c72f
leovct <l009.vincent@gmail.com>
2022-12-08 10:34:44 +0100

    ci: let's keep it simple

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 # This script allows to build all the Rust projects contained in this monorepo
-# Usage: ./scripts/ci.sh [-v|--verbose]
 
 # Run cargo build command for every path in a list
 build() {
   while read path; do
     echo "Project: $path"
-		arg="${args[0]}"
-		if [[ "$arg" == "--verbose" || "$arg" == "-v" ]]; then
-			cargo build --verbose --manifest-path "$path"
-		else
-			cargo build --manifest-path "$path"
-		fi
+		cargo build --verbose --manifest-path "$path"
   done
 }
-
-args="$@"
 
 # Find all the directories containing a Cargo.toml file
 find . -name 'Cargo.toml' | sort | build
