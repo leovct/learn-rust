523a8fe249c3f38fe88f6f91a360d80a37881449
leovct <l009.vincent@gmail.com>
2022-12-08 10:31:56 +0100

    ci: fix script again again

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This script allows to build all the Rust projects contained in this monorepo
-# Usage:
-# $ ./scripts/ci.sh
-# $ ./scripts/ci.sh --verbose # with verbosity
+# Usage: ./scripts/ci.sh [-v|--verbose]
 
 # Run cargo build command for every path in a list
 build() {
@@ -10,9 +8,9 @@ build() {
     echo "Project: $path"
 		arg="${args[0]}"
 		if [[ "$arg" == "--verbose" || "$arg" == "-v" ]]; then
-			cargo build --verbose --manifest-path $path
+			cargo build --verbose --manifest-path "$path"
 		else
-			cargo build --manifest-path $path
+			cargo build --manifest-path "$path"
 		fi
   done
 }
