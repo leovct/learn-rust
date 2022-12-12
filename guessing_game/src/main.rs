717ffe75901c1b8656c4fdecaef9177c1aafda36
leovct <l009.vincent@gmail.com>
2022-12-12 08:18:45 +0100

    chore: fix indentation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -23,46 +23,46 @@ fn main() {
     loop {
 			println!("Please input your guess.");
 
-		// Rust variables are immutable by default (value won't change)
-		// Note that references and pointers are also immutable!!
-		// To make a variable mutable, we add mut before the variable name
-    let mut guess = String::new();
+			// Rust variables are immutable by default (value won't change)
+			// Note that references and pointers are also immutable!!
+			// To make a variable mutable, we add mut before the variable name
+			let mut guess = String::new();
 
-		// Read the user input
-		// read_line returns a Result value (enum) which can either be equal to Ok or Err
-		// - If Err is returned, expect will throw an error
-		// - If Ok is returned, the value will be stored in the guess variable
-    io::stdin()
-        .read_line(&mut guess) // Since references are immutable by default, we need to add mut here
-        .expect("Failed to read line");
+			// Read the user input
+			// read_line returns a Result value (enum) which can either be equal to Ok or Err
+			// - If Err is returned, expect will throw an error
+			// - If Ok is returned, the value will be stored in the guess variable
+			io::stdin()
+					.read_line(&mut guess) // Since references are immutable by default, we need to add mut here
+					.expect("Failed to read line");
 
-		// Check that the input is a number (it shadows the other guess variable)
-		// - trim() removes all the whitespaces
-		// - parse() converts a string to another type, here u32 for an unsigned 32-bit integer
-		let guess: u32 = match guess.trim().parse() {
-			Ok(num) => {
-				trials += 1;
-				num
-			},
-			Err(_) => {
-				println!("Please input an integer number!");
-				continue
-			},
-		};
-		
-		// Use placeholders
-		// Or also: println!("You guessed: {}", guess);
-		println!("You guessed: {guess}");		
+			// Check that the input is a number (it shadows the other guess variable)
+			// - trim() removes all the whitespaces
+			// - parse() converts a string to another type, here u32 for an unsigned 32-bit integer
+			let guess: u32 = match guess.trim().parse() {
+				Ok(num) => {
+					trials += 1;
+					num
+				},
+				Err(_) => {
+					println!("Please input an integer number!");
+					continue
+				},
+			};
+			
+			// Use placeholders
+			// Or also: println!("You guessed: {}", guess);
+			println!("You guessed: {guess}");		
 
-		// Compare values
-		match guess.cmp(&secret_number) {
-			Ordering::Less => println!("Too small!"),
-			Ordering::Greater => println!("Too big"),
-			Ordering::Equal => {
-				println!("You won in {trials} trials!");
-				break;
-			},
-		}
+			// Compare values
+			match guess.cmp(&secret_number) {
+				Ordering::Less => println!("Too small!"),
+				Ordering::Greater => println!("Too big"),
+				Ordering::Equal => {
+					println!("You won in {trials} trials!");
+					break;
+				},
+			}
 		}
 
 }
